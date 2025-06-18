46c72c5fdf4612a43042270e4b574aebe9030f7a
jesusmartinvz <jesus_martim@hotmail.com>
2025-06-17 21:03:32 -0500

    advance

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import { Environment } from "./environment.interface";
 
 export const environment: Environment = {
     production: false,
-    apiUrlBase: 'http://172.191.184.95:8088',
+    apiUrlBase: 'https://jackgptgod-001-site1.ktempurl.com',
     siteKeyCaptcha: '6Le_914rAAAAAEIpvmvQu_r8561iDthHJuTuwp6K'
 };
