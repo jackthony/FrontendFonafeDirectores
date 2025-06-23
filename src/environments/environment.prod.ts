581c8fd063e6dca02a6beebdfc3780881bec03f3
AxelValuis <banel1306@hotmail.es>
2025-06-23 06:11:58 -0500

    CAMBIO URL PRODUCCION

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import { Environment } from "./environment.interface";
 export const environment: Environment = {
     production: false,
     //apiUrlBase: 'https://172.191.184.95:8088',
-    apiUrlBase: 'https://jackgptgod-001-site1.ktempurl.com',
+    apiUrlBase: 'https://jackgptgod-001-site1.ktempurl.com/api',
     siteKeyCaptcha: '6Le_914rAAAAAEIpvmvQu_r8561iDthHJuTuwp6K'
 };
